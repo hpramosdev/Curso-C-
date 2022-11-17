ec9cea62cda5af62efd7b5001ac285bb4fa63e05
hpramosdev <henrique.pramos@gmail.com>
2022-11-17 19:33:12 -0300

    Resolução de atividade

---
+++ Exercício 1/N 4/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Digite o número do funcionário: ");
+string funcionario = Console.ReadLine();
+
+Console.Write("Insira o número de horas trabalhadas pelo funcionário: ");
+int horas = int.Parse(Console.ReadLine());
+
+Console.Write("Insira o valor da hora do funcionário: ");
+double valor = double.Parse(Console.ReadLine());
+double salario = horas * valor;
+
+Console.WriteLine($"Funcionário: {funcionario} \nSalário: {salario:F2}");
