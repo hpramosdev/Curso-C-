d5e083d0ed4cd1072b842a8a78224fec20e5b5c5
hpramosdev <henrique.pramos@gmail.com>
2022-11-17 11:29:24 -0300

    Exercício 2

---
+++ Exercício 2/Program2.cs
@@ -0,0 +1,5 @@
+﻿Console.Write("Insira o valor do raio de um círculo para calcular sua área: ");
+double Pi = 3.14159;
+double Raio = double.Parse(Console.ReadLine());
+double Area = Pi * (Raio * Raio);
+Console.WriteLine($"{Area:F4}");
