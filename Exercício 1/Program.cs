d0a9125733dac60b1c4d6d4dbb9a75758b34781f
hpramosdev <henrique.pramos@gmail.com>
2022-11-17 11:04:45 -0300

    Exercício 1

---
+++ Exercício 1/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Insira dois números inteiros");
+
+Console.Write("número 1: ");
+int num1 = int.Parse(Console.ReadLine());
+
+Console.Write("número 2: ");
+int num2 = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"SOMA = {num1 + num2}");
