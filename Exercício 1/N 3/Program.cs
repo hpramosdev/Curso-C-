bbec0634aced0eb39e7cece3e43ccdee02eb2383
hpramosdev <henrique.pramos@gmail.com>
2022-11-17 13:44:31 -0300

    Resolução de exercício

---
+++ Exercício 1/N 3/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("insira 4 números para identificar a diferença entre os produtos de A e B com C e D:");
+string s = Console.ReadLine();
+string[] vet = s.Split(' ');
+int diferenca = (int.Parse(vet[0]) * int.Parse(vet[1])) - (int.Parse(vet[2]) * int.Parse(vet[3]));  
+Console.WriteLine(diferenca);
