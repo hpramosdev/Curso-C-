bced3c15ab2459396a77f3b2d294d34d694833be
hpramosdev <henrique.pramos@gmail.com>
2022-11-25 14:43:44 -0300

    Resolução de atividade

---
+++ Exercício 1/N 6/Program.cs
@@ -0,0 +1,46 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Insira 3 valores com ponto flutuante: ");
+string s = Console.ReadLine();
+string[] vet =  s.Split(' ');
+
+double triangulo = Triangulo(vet);
+double circulo = Circulo(vet);
+double trapezio = Trapezio(vet);
+double quadrado = Quadrado(vet);
+double retangulo = Retangulo(vet);
+
+Console.Write($"TRIANGULO: {triangulo:F3} \nCIRCULO: {circulo:F3}" + 
+              $"\nTRAPÉZIO: {trapezio:F3} \nQUADRADO: {quadrado:F3}" +
+              $"\nRETANGULO: {retangulo:F3}");
+
+
+double Triangulo(string[] vet)
+{
+    double result = (double.Parse(vet[0]) * double.Parse(vet[2])) / 2;
+    return result;
+}
+
+double Circulo(string[] vet)
+{
+    double pi = 3.14159;
+    double result = pi * (double.Parse(vet[2]) * double.Parse(vet[2]));
+    return result;
+}
+
+double Trapezio(string[] vet)
+{
+    double result = (double.Parse(vet[2]) * (double.Parse(vet[0]) + double.Parse(vet[1]))) / 2;
+    return result;
+}
+
+double Quadrado(string[] vet)
+{
+    double result = double.Parse(vet[1]) * double.Parse(vet[1]);
+    return result;
+}
+
+double Retangulo(string[] vet)
+{
+    double result = double.Parse(vet[0]) * double.Parse(vet[1]);
+    return result;
+}
