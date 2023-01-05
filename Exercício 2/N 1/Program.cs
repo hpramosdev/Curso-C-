d3f58dcb7aae401a9ea1a03bb4004a34d80dd88c
hpramosdev <henrique.pramos@gmail.com>
2023-01-05 09:51:23 -0300

    atualização ambiente

---
+++ Exercício 2/N 1/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Insira um número inteiro para saber se é positivo ou negativo: ");
+int n = int.Parse(Console.ReadLine());
+
+if (n < 0) {
+    Console.WriteLine("Negativo");
+} else {
+    Console.WriteLine("Positivo"); 
+}
